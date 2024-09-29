41a25aa0f3a2a31d63b7e5f2c723d03b1804b847
Bhanoo Vishwakrma <bhanoolohar007@gmail.com.com>
2024-09-29 13:11:08 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+// COnfigurations
+dotenv.config();
+
+
+app.use(cors({
+	origin: [
+		'http://localhost:3000', 
+		'https://your-frontend-domain.com'
+	],
+	credentials: true,
+}));
+  
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
